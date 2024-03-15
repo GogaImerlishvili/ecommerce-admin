2eeb33ea20b50b02ecf8678ef7bb0da24facfaab
GogaImerlishvili <gogaimerlishvili1@gmail.com>
2024-03-16 00:19:02 +0400

    add navbar component for all page

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import Header from "./components/layour/Header";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -11,7 +12,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main className="max-w-4xl mx-auto  p-4">
+          <Header />
+        {children}
+        </main>
+        </body>
     </html>
   );
 }
