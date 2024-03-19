a036f9b717a02c2c5f00a25b3ac7f0e7508a4b01
GogaImerlishvili <gogaimerlishvili1@gmail.com>
2024-03-19 15:56:46 +0400

    create header

---
+++ src/app/components/layout/Header.js
@@ -0,0 +1,48 @@
+"use client";
+import { signOut, useSession } from "next-auth/react";
+import Link from "next/link";
+
+const Header = () => {
+  const session = useSession();
+  console.log(session);
+  const status = session.status;
+  return (
+    <>
+      <header className="flex items-center justify-between">
+        <nav className="flex gap-8 items-center text-gray-500 font-semibold">
+          <Link className="text-red-700 font-semibold text-2xl" href="/">
+            Logo
+          </Link>
+          <Link href={"/"}>Home</Link>
+          <Link href={""}>Menu</Link>
+          <Link href={""}>About</Link>
+          <Link href={""}>Contact</Link>
+        </nav>
+        <nav className="flex items-center gap-4 text-gray-500 font-semibold">
+          {status === "authenticated" && (
+            <button
+            onClick={() => signOut()}
+              href={"/register"}
+              className="bg-red-600 text-white rounded-full px-8 py-2"
+            >
+              Logout
+            </button>
+          )}
+          {status === "unauthenticated" && (
+            <>
+              <Link href={"/login"}>Login</Link>
+              <Link
+                href={"/register"}
+                className="bg-red-600 text-white rounded-full px-8 py-2"
+              >
+                Register
+              </Link>
+            </>
+          )}
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default Header;
