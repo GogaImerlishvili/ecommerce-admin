c4b1b378eae84d59463da6b7ee971317d8714a84
GogaImerlishvili <gogaimerlishvili1@gmail.com>
2024-03-20 20:50:47 +0400

    add menu section

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,46 @@
-import Hero from "@/app/components/layout/Hero"
+import Hero from "@/app/components/layout/Hero";
+import HomeMenu from "@/app/components/layout/HomeMenu";
+import SectionHeaders from "./components/layout/SectionHeaders";
 
 export default function Home() {
   return (
-  <>
-  <Hero />
-  </>
+    <>
+      <Hero />
+      <HomeMenu />
+      <section className="text-center my-16">
+        <SectionHeaders subHeader={"Our story"} mainHeader={"About us"} />
+        <div className="text-gray-500 max-w-md mx-auto mt-4 flex flex-col gap-4">
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s.
+          </p>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s.
+          </p>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s.
+          </p>
+        </div>
+      </section>
+      <section className="text-center my-8">
+        <SectionHeaders
+          subHeader={"Don't hesitate"}
+          mainHeader={"Contact us"}
+        />
+        <div className="mt-8">
+          <a href="tel:+995514824584" className="text-4xl underline text-gray-500">
+            +995 514 82 45 84
+          </a>
+        </div>
+      </section>
+      <footer className="border-t p-8 text-center text-gray-500 mt-16">
+        &copy; 2024 All rights reserved
+      </footer>
+    </>
   );
 }
